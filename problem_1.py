dc35c4c56a734a9cc4d4770eda31903fc600fe50
dharshan736 <dharshan070306@gmail.com>
2024-12-08 22:56:29 +0530

    Create problem_1.py

---
+++ problem_1.py
@@ -0,0 +1,16 @@
+def calculate_max_profit(prices):
+    min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        profit = price - min_price
+        if profit > max_profit:
+            max_profit = profit
+
+    return max_profit
+
+n = int(input())
+prices = list(map(int,input().split()))
+result = calculate_max_profit(prices)
+print(result)
